d998ce6ab55b12368c0f93f6e6b2ab71322bc7f4
DayanaGamboaM <mongeg1715@gmail.com>
2023-01-23 20:18:06 -0600

    Agrego clases vacias

--- sfmlCatan1/GamePiece.cpp
+++
@@ -1,7 +0,0 @@
-#include "GamePiece.h"
-
-GamePiece::GamePiece(int ptotalPieces, int ppieces)
-{
-	this->totalPieces = ptotalPieces;
-	*this->pieces = ppieces;
-}
--- sfmlCatan1/Land.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "GamePiece.h"
-class Land//: public GamePiece
-{
-
-};
-
