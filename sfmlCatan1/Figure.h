d40d1efc68ec8bfdc4c2d021d964e8ed3c9cf89b
Maria-Chacon <123408968+Maria-Chacon@users.noreply.github.com>
2023-02-02 00:14:51 -0600

    implementation of the abstract class Figure

--- sfmlCatan1/Figure.h
+++ sfmlCatan1/Figure.h
@@ -4,6 +4,13 @@ class Figure
 public:
 	Figure();
 	~Figure();
+	virtual string toString() = 0;
+	string getCardName();
+	void setCardName(string);
+	int  getPositionX();
+	void setPositionX(int);
+	int  getPositionY();
+	void setPositionY(int);
 
 private:
 	int quantity;
