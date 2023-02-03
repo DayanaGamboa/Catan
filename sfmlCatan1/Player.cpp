02bd36db66d54fd9d729ccf2a3dc670ded80b2c7
Arianna Aguilar Leiva <arianna.aguilar.leiva@est.una.ac.cr>
2023-02-02 21:42:20 -0600

    Funtion paintPlayerData and addition to the menu

--- sfmlCatan1/Player.cpp
+++ sfmlCatan1/Player.cpp
@@ -1,5 +1,4 @@
 #include "Player.h"
-#include<sstream>
 
 
 Player::Player(int id, string name, int age, int victoryPoint, int color)
@@ -14,6 +13,9 @@ Player::Player(int id, string name, int age, int victoryPoint, int color)
 	developmentCardList = new CircularList<DevelopmentCard>;
 	specialCardList = new CircularList<SpecialCard>;
 	//figurelist = new CircularList<Figure>;
+    if (!font.loadFromFile("Fonts/Wintertime.ttf")) {
+        cout << "¡¡No Font!!";
+    }
 }
 
 Player::~Player()
@@ -130,7 +132,114 @@ void Player::insertSpecialCard(string cardNameP, int powerCardP, int positionXP,
 	SpecialCard* info = new SpecialCard(cardNameP, powerCardP, positionXP, positionYP);
 	specialCardList->inserNode(info);
 }
+void Player::paintPlayerRegister(RenderWindow* playerData) {
+    
+    //MENU
+    fondo.setSize(Vector2f(700, 700));
+    fondo.setFillColor(Color(220, 245, 255));
+
+    titulo1.setFont(font);
+    titulo1.setString("Registro de jugadores: (3-4)");
+    titulo1.setFillColor(Color::Black);
+    titulo1.setPosition(200, 10);
+    titulo1.setCharacterSize(50);
+
+    //Boton de aceptar    
+    rtsBtnAceptar.setPosition(Vector2f(300, 600));
+    rtsBtnAceptar.setSize(Vector2f(120, 50));  
+    txtrBtnAceptar.loadFromFile("resouceImages/btnSave.png");
+    rtsBtnAceptar.setTexture(&txtrBtnAceptar);
+
+    //JUGADOR 1    
+    tituloJugador.setFont(font);    
+    tituloJugador.setFillColor(Color::Black);
+    tituloJugador.setPosition(300, 160);
+
+    //titulo ID    
+    tituloID.setFont(font);
+    tituloID.setString("ID ");
+    tituloID.setFillColor(Color::Black);
+    tituloID.setPosition(180, 230 + 10);
+
+    // Caja de texto ID   
+    rtsID.setSize(Vector2f(200, 50));
+    rtsID.setFillColor(Color(187, 208, 216));
+    rtsID.setPosition(250, 230);
+
+    //Texto para la caja ID    
+    txtID.setFont(font);    
+    txtID.setFillColor(Color::Black);
+    txtID.setPosition(250, 230);
+
+    //titulo nombre   
+    tituloNombre.setFont(font);
+    tituloNombre.setString("Nombre ");
+    tituloNombre.setFillColor(Color::Black);
+    tituloNombre.setPosition(180, 310);
+
+    //Caja de texto nombre  
+    rtsNombre.setSize(Vector2f(200, 50));
+    rtsNombre.setFillColor(Color(187, 208, 216));
+    rtsNombre.setPosition(250, 300);
+
+    //Texto para la caja nombre    
+    txtNombre.setFont(font);  
+    txtNombre.setFillColor(Color::Black);
+    txtNombre.setPosition(250, 300);
+
+    //titulo edad    
+    tituloEdad.setFont(font);
+    tituloEdad.setString("Edad ");
+    tituloEdad.setFillColor(Color::Black);
+    tituloEdad.setPosition(180, 380);
+
+    //Caja de texto Edad    
+    rtsEdad.setSize(Vector2f(200, 50));
+    rtsEdad.setFillColor(Color(187, 208, 216));
+    rtsEdad.setPosition(250, 370);
+
+    //Texto para la caja Edad  
+    txtEdad.setFont(font);   
+    txtEdad.setFillColor(Color::Black);
+    txtEdad.setPosition(250, 370);
+
+    //titulo color  
+    tituloColor.setFont(font);
+    tituloColor.setString("Color");
+    tituloColor.setFillColor(Color::Black);
+    tituloColor.setPosition(180, 460);
+
+    //Caja de texto Color  
+    rtsColor.setSize(Vector2f(200, 50));   
+    rtsColor.setPosition(250, 450);
+
+    //Text para campos requeridos   
+    txtCamposRequeridos.setFont(font);   
+    txtCamposRequeridos.setFillColor(Color::Black);
+    txtCamposRequeridos.setPosition(250, 500);
 
 
+    playerData->draw(fondo);
+    playerData->draw(titulo1);
+    playerData->draw(rtsBtnAceptar);
 
+    playerData->draw(tituloJugador);
 
+    playerData->draw(tituloID);
+    playerData->draw(rtsID);
+    playerData->draw(txtID);
+
+    playerData->draw(tituloNombre);
+    playerData->draw(rtsNombre);
+    playerData->draw(txtNombre);
+
+    playerData->draw(tituloEdad);
+    playerData->draw(rtsEdad);
+    playerData->draw(txtEdad);
+
+    playerData->draw(tituloColor);
+    playerData->draw(rtsColor);
+
+    playerData->draw(txtCamposRequeridos);
+
+}
--- sfmlCatan1/Player.h
+++ sfmlCatan1/Player.h
@@ -9,6 +9,12 @@
 #include "City.h"
 #include "Street.h"
 #include "Town.h"
+#include "StartWindow.h"
+#include "SFML/Graphics.hpp"
+#include<sstream>
+
+using namespace std;
+using namespace sf;
 class Player
 {
 public:
@@ -36,6 +42,9 @@ public:
 	void insertDevelopmentCard(string, int, int, int, int);
 	void insertSpecialCard(string, int, int, int);
 	void showCard();
+
+	
+	void paintPlayerRegister(RenderWindow*);
 	
 	
 	CircularList<ResourceCard>* resourceCardsList;
@@ -44,7 +53,25 @@ public:
 	CircularList<City>* cityList;
 	CircularList<Street>* streetList;
 	CircularList<Town>* townList;
-
+	
+	RectangleShape rtsBtnAceptar;
+	Text tituloJugador;
+	Text titulo1;
+	Text tituloID;
+	RectangleShape rtsID;
+	Text txtID;
+	Text tituloNombre;
+	RectangleShape rtsNombre;
+	Text txtNombre;
+	Text tituloEdad;
+	RectangleShape rtsEdad;
+	Text txtEdad;
+	Text tituloColor;
+	RectangleShape rtsColor;
+	Text txtCamposRequeridos;
+	RectangleShape fondo;
+	Texture txtrBtnAceptar;
+	
 	
 private:
 	int id;
@@ -52,6 +79,9 @@ private:
 	int age;
 	int victoryPoint;
 	int color;
+	Font font;
+	Event event;
+	Mouse mouse;
 	//Deck deck[]; // Debería de ser una lista?
 
 };
