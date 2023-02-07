c383054478a69fd6971b2997419e44de8ed1133e
Maria-Chacon <123408968+Maria-Chacon@users.noreply.github.com>
2023-02-07 16:32:06 -0600

    method to save the data of the figures that the player in turn has and display it on the screen

--- sfmlCatan1/StartWindow.h
+++ sfmlCatan1/StartWindow.h
@@ -33,6 +33,7 @@ public:
 	Node<Player>* nodePlayer3;
 	Node<Player>* nodePlayer4;
 	Node<ResourceCard>* nodeResource;
+	Node<Figure>* nodeFigure;
 	Node<ResourceCard>* nodeResourceBankWood;
 	Node<ResourceCard>* nodeResourceBankSheep;
 	Node<ResourceCard>* nodeResourceBankMineral;
@@ -69,6 +70,7 @@ public:
 	void paintBankCounters(RenderWindow*);
 	void paintPlayerCountersInTurn(RenderWindow*);
 	void saveResourcePlayer();
+	void saveFigfurePlayer();
 	void saveResourceBank();
 	void paintCountersFigures(RenderWindow*);
 
@@ -102,7 +104,7 @@ public:
 	int LandsPosX[19] = { 753,672,589,552,512,550,589,670,751,791,831,791,711,632,592,632,711,750,672 };
 	int LandsPosY[19] = { 228,228,228,285,344,401,457,457,457,401,344,285,285,285,344,401,401,343,343 };
 	int woodR = 0, clayR = 0, mineralR = 0, sheepR = 0, cerealR = 0;
-	
+	int streetF = 0, townF = 0, cityF = 0;
 
 private:	
 	Font font;
